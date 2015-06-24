8f0dbd1286b937400dc4dd94dbb19f35e78c5a05
Mike Bostock <mbostock@gmail.com>
2015-06-24 11:15:50 -0700

    Add interval.round.

--- src/interval.js
+++ src/interval.js
@@ -3,6 +3,12 @@ export default function(floor, step) {
     floor: function(date) {
       return floor(date = new Date(+date)), date;
     },
+    round: function(date) {
+      var d0 = new Date(+date),
+          d1 = new Date(date - 1);
+      floor(d0), floor(d1), step(d1, 1);
+      return date - d0 < d1 - date ? d0 : d1;
+    },
     ceil: function(date) {
       return floor(date = new Date(date - 1)), step(date, 1), date;
     }

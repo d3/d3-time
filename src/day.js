72865054e1106ad7b0409400a167441103170f13
Mike Bostock <mbostock@gmail.com>
2015-06-28 14:25:35 -0700

    Fix day.floor (#1).
    However, this does not help #2 or #3 because offset does not preserve the delta
    between the date and the closest boundary, so range and ceil may sometimes not
    return boundary dates. Also, this doesn’t fix week and month.

--- src/day.js
+++ src/day.js
@@ -1,7 +1,9 @@
 import interval from "./interval";
 
 export default interval(function(date) {
+  var date0 = date.getDate(), offset0 = date.getTimezoneOffset();
   date.setHours(0, 0, 0, 0);
+  if (date.getDate() !== date0) date.setTime(date.getTime() + (date.getTimezoneOffset() - offset0) * 6e4);
 }, function(date, offset) {
   date.setDate(date.getDate() + offset);
 }, function(start, end) {

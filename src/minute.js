7e8826461306fe3d49893552e87c94add707ecfe
Mike Bostock <mbostock@gmail.com>
2015-06-29 12:02:39 -0700

    Implement {minute,second}.ceil.

--- src/minute.js
+++ src/minute.js
@@ -2,6 +2,10 @@ import interval from "./interval";
 
 export default interval(function(date) {
   date.setSeconds(0, 0);
+}, function(date) {
+  date.setTime(date - 1);
+  date.setSeconds(0, 0);
+  date.setTime(+date + 6e4);
 }, function(date, offset) {
   date.setTime(+date + offset * 6e4);
 }, function(start, end) {
--- src/second.js
+++ src/second.js
@@ -2,6 +2,10 @@ import interval from "./interval";
 
 export default interval(function(date) {
   date.setMilliseconds(0);
+}, function(date) {
+  date.setTime(date - 1);
+  date.setMilliseconds(0);
+  date.setTime(+date + 1e3);
 }, function(date, offset) {
   date.setTime(+date + offset * 1e3);
 }, function(start, end) {

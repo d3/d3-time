81f333c434e54deebb6015b31bef35052e8fccd8
Mike Bostock <mbostock@gmail.com>
2015-06-29 08:46:51 -0700

    Fix for irregular day boundaries.
    Fixes #1 #2 #3. Also breaks all other intervals, since they need to have a
    ceiling function now, too. Also doesn’t fix day.offset, but I’m not totally sure
    that’s fixable yet and if so what the behavior should be.

--- src/day.js
+++ src/day.js
@@ -4,6 +4,10 @@ export default interval(function(date) {
   var date0 = date.getDate(), offset0 = date.getTimezoneOffset();
   date.setHours(0, 0, 0, 0);
   if (date.getDate() !== date0) date.setTime(date.getTime() + (date.getTimezoneOffset() - offset0) * 6e4);
+}, function(date) {
+  var date0 = date.getDate(), offset0 = date.getTimezoneOffset();
+  date.setHours(24, 0, 0, 0);
+  if (date.getDate() === date0) date.setTime(date.getTime() - (new Date(+date + 432e5).getTimezoneOffset() - offset0) * 6e4);
 }, function(date, offset) {
   date.setDate(date.getDate() + offset);
 }, function(start, end) {
--- src/interval.js
+++ src/interval.js
@@ -1,4 +1,4 @@
-export default function newInterval(floori, offseti, count) {
+export default function newInterval(floori, ceili, offseti, count) {
 
   function interval(date) {
     return floori(date = new Date(+date)), date;
@@ -9,12 +9,12 @@ export default function newInterval(floori, offseti, count) {
   interval.round = function(date) {
     var d0 = new Date(+date),
         d1 = new Date(date - 1);
-    floori(d0), floori(d1), offseti(d1, 1);
+    floori(d0), ceili(d1);
     return date - d0 < d1 - date ? d0 : d1;
   };
 
   interval.ceil = function(date) {
-    return floori(date = new Date(date - 1)), offseti(date, 1), date;
+    return ceili(date = new Date(date - 1)), date;
   };
 
   interval.offset = function(date, step) {
@@ -23,13 +23,14 @@ export default function newInterval(floori, offseti, count) {
 
   interval.range = function(start, stop, step) {
     var range = [];
-    start = new Date(start - 1);
+    start = new Date(+start);
     stop = new Date(+stop);
     step = step == null ? 1 : Math.floor(step);
-    if (!(start < stop)) return range; // also handles Invalid Date
-    floori(start), offseti(start, 1);
-    if (start < stop) range.push(new Date(+start));
-    while (offseti(start, step), start < stop) range.push(new Date(+start));
+    while (ceili(start), start < stop) {
+      range.push(new Date(+start));
+      for (var i = step; i > 1; --i) start.setTime(+start + 1), ceili(start);
+      start.setTime(+start + 1);
+    }
     return range;
   };
 

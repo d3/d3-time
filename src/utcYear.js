a31e23860ac2163a902b30ca7becc74b5bbb6a73
Mike Bostock <mbostock@gmail.com>
2016-06-13 10:39:44 -0700

    Optimize {timeYear,utcYear}.every.
    Related d3/d3-scale#65.

--- src/utcYear.js
+++ src/utcYear.js
@@ -11,5 +11,16 @@ var utcYear = interval(function(date) {
   return date.getUTCFullYear();
 });
 
+// An optimized implementation for this simple case.
+utcYear.every = function(k) {
+  return !isFinite(k = Math.floor(k)) || !(k > 0) ? null : interval(function(date) {
+    date.setUTCFullYear(Math.floor(date.getUTCFullYear() / k) * k);
+    date.setUTCMonth(0, 1);
+    date.setUTCHours(0, 0, 0, 0);
+  }, function(date, step) {
+    date.setUTCFullYear(date.getUTCFullYear() + step * k);
+  });
+};
+
 export default utcYear;
 export var utcYears = utcYear.range;
